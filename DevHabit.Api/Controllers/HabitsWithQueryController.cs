bd1de5ca5cd3b3c27f7e7ff3ec3b28b4cc83a327
dimasrw34 <boyarskidm@yandex.ru>
2025-04-18 19:27:03 +0300

    23 lesson ended searching and sorting

---
+++ DevHabit.Api/Controllers/HabitsWithQueryController.cs
@@ -0,0 +1,172 @@
+using System.Linq.Dynamic.Core;
+using System.Linq.Expressions;
+using DevHabit.Api.Database;
+using DevHabit.Api.DTOs.Habits;
+using DevHabit.Api.DTOs.HabitTags;
+using DevHabit.Api.Entities;
+using DevHabit.Api.Services.Sorting;
+using FluentValidation;
+using Microsoft.AspNetCore.JsonPatch;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevHabit.Api.Controllers;
+
+[ApiController]
+[Route("habits_q")]
+public class HabitsWithQueryController(ApplicationDbContext dbContext) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<HabitsCollectionDto>> GetHabits(
+        [FromQuery] HabitsQueryParameters query,
+        SortMappingProvider sortMappingProvider)
+    {
+        if (!sortMappingProvider.ValidateMappings<HabitDto, Habit>(query.Sort))
+        {
+            return Problem(
+                statusCode: StatusCodes.Status400BadRequest,
+                detail: $"The provider sort parameter isn't valid: {query.Sort}");
+        }
+
+        query.Search ??= query.Search?.Trim().ToLower();
+        
+        SortMapping[] sortMappings = sortMappingProvider.GetMappings<HabitDto, Habit>();
+        
+
+        /*IQueryable<Habit> query = dbContext.Habits;
+
+        if (!string.IsNullOrWhiteSpace(search))
+        {
+            query = query.Where(h => h.Name.ToLower().Contains(search) ||
+                                     h.Description != null && h.Description.ToLower().Contains(search));
+        }
+
+        List<HabitDto> habits = await query
+            .Select(HabitQueries.ProjectToDto())
+            .ToListAsync();*/
+        
+        //refactored up here code 
+        Expression<Func<Habit, object>> orderBy = query.Sort switch
+        {
+            "name" => h => h.Name,
+            "description" => h => h.Description,
+            "type" => h => h.Type,
+            "status" => h => h.HabitStatus,
+            _ => h => h.Name
+        };
+        
+        List<HabitDto> habits = await dbContext
+            .Habits
+            .Where(h => query.Search == null ||
+                        h.Name.ToLower().Contains(query.Search) ||
+                        h.Description != null && h.Description.ToLower().Contains(query.Search))
+            .Where((h => query.Type == null || h.Type == query.Type))
+            .Where(h => query.Status == null || h.HabitStatus == query.Status)
+            .ApplySort(query.Sort, sortMappings)
+            .Select(HabitQueries.ProjectToDto())
+            .ToListAsync();
+
+        var habitsCollectionDto = new HabitsCollectionDto
+        {
+            Data = habits
+        };
+        return Ok(habitsCollectionDto);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<HabitWithTagsDto>> GetHabit(string id)
+    {
+        HabitWithTagsDto? habit = await dbContext
+            .Habits
+            .Where(h => h.Id == id)
+            .Select(HabitQueries.ProjectToHabitWithTagsDto())
+            .FirstOrDefaultAsync();
+        if (habit is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(habit);
+    }
+
+
+    [HttpPost]
+    public async Task<ActionResult<HabitDto>> CreateHabit(
+        CreateHabitDto createHabitDto, 
+        IValidator<CreateHabitDto> validator)
+    {
+        
+        //Закоментирован, так как используется ValidationHandler (middleware)
+        /*ValidationResult validationResult = await validator.ValidateAsync(createHabitDto);
+
+        if (!validationResult.IsValid)
+        {
+            return BadRequest(validationResult.ToDictionary());
+        }*/
+        await validator.ValidateAndThrowAsync(createHabitDto);
+
+        var habit = createHabitDto.ToEntity();
+        dbContext.Habits.Add(habit);
+        await dbContext.SaveChangesAsync();
+
+        HabitDto habitDto = habit.ToDto();
+
+        return CreatedAtAction(nameof(GetHabit), new { id = habitDto.Id }, habitDto);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> UpdateHabit(string id, UpdateHabitDto updateHabitDto)
+    {
+        Habit? habit = await dbContext.Habits.FirstOrDefaultAsync(h => h.Id == id);
+        if (habit is null)
+        {
+            return NotFound();
+        }
+
+        habit.UpdateFromDto(updateHabitDto);
+
+        await dbContext.SaveChangesAsync();
+        return NoContent();
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<ActionResult> PatchHabit(string id, JsonPatchDocument<HabitDto> patchDocument)
+    {
+        Habit? habit = await dbContext.Habits.FirstOrDefaultAsync(h => h.Id == id);
+        if (habit is null)
+        {
+            return NotFound();
+        }
+
+        HabitDto habitDto = habit.ToDto();
+        patchDocument.ApplyTo(habitDto, ModelState);
+        if (!ModelState.IsValid)
+        {
+            return ValidationProblem(ModelState);
+        }
+
+        habit.Name = habitDto.Name;
+        habit.Description = habitDto.Description;
+        habit.UpdatedAtUtc = DateTime.UtcNow;
+
+        await dbContext.SaveChangesAsync();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteHabit(string id)
+    {
+        Habit? habit = await dbContext.Habits.FirstOrDefaultAsync(h => h.Id == id);
+
+        if (habit is null)
+        {
+            return StatusCode(StatusCodes.Status410Gone);
+        }
+
+        dbContext.Habits.Remove(habit);
+
+        await dbContext.SaveChangesAsync();
+        return NoContent();
+    }
+}
